3e99fc81d2607b6bcfc1ec4924bae095a3c74978
Tom Browder <tom.browder@gmail.com>
2016-03-29 14:59:17 -0500

    start a Webmin ssg tunnel script

---
+++ create-ssh-tunnel-webmin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# run to establish an ssh tunnel to run Webmin on a Debian-based host
+
+# to enter webmin, use a browser and navigate to:
+#    https://localhost:10000
+
+if [[ -z "$1" ]] ; then
+  echo "Usage: $0 <host> <ssh user> <ssh password>"
+  echo
+  echo "Runs an ssh tunnel to the remote host."
+  echo "Then use a browser to navigate to 'https://localhost:10000'"
+  echo
+  exit
+fi
+
+HOST=$1
+USR=$2
+PASSWD=$3
+
+ssh -L 15000:localhost:10000 $USR\@$HOST:10000 
