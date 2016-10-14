4fb9608f6d9607014694d96231c4dfb0d17a5d7f
Tom Browder <tom.browder@gmail.com>
2016-10-14 07:25:02 -0500

    allow for more options

--- install-rakudo-star.sh
+++ install-rakudo-star.sh
@@ -9,21 +9,28 @@
 # requires bash 3.2 (e.g., bash --version)
 check_bash_version
 
-ARCH=star-2016.07.tar.gz
+ARCH=rakudo-star-2016.07.tar.gz
 ARCHDIR=`get_archdir $ARCH`
 
-echo "ARCH: '$ARCH'";
-echo "ARCHDIR: '$ARCHDIR'";
-debug_exit 3
+#echo "ARCH: '$ARCH'";
+#echo "ARCHDIR: '$ARCHDIR'";
+#debug_exit 3
 
 INSTDIR=/usr/local/rakudo.d
 BINDIR=$INSTDIR/bin
+
+GO=
+TEST=
+DEBUG=
+YES=
+UNK=
+
 if [[ -z "$1" ]] ; then
     cat <<EOF
-Usage: $0 go
+Usage: $0 go | [-test][-debug][-yes]
 
 As root, sets up a new Rakudo installation using a tgz release
-  from <https://github.com/rakudo/star>.
+  from <http://rakudo.org/downloads/star>.
 
 Start in a clean directory with the desired Rakudo Star archive.
 
@@ -34,25 +41,127 @@ desired:
   INSTDIR - '$INSTDIR'
   BINDIR  - '$BINDIR'
 
+Options:
+
+  -test    No commands are executed.
+  -debug   Extra information is printed.
+  -yes     Answers 'y' to all questions (DANGER)
 EOF
   exit 0
 fi
 
+# get input vars first
+for o in $@
+do
+    echo "input var '$o'"
+case $o in
+    -g|g|-go)
+    GO=1
+    shift # past argument=value
+    ;;
+    -t|t|-test)
+    TEST=1
+    GO=1
+    shift # past argument=value
+    ;;
+    -d|d|-debug)
+    DEBUG=1
+    GO=1
+    shift # past argument=value
+    ;;
+    -y|y|-yes)
+    YES=1
+    GO=1
+    shift # past argument=value
+    ;;
+    *)
+    UNK=$o # unknown option
+    shift # past argument=value
+    ;;
+esac
+done
+
+if [[ -n $DEBUG ]] ; then
+    echo "GO    = '$GO'"
+    echo "TEST  = '$TEST'"
+    echo "YES   = '$YES'"
+    echo "DEBUG = '$DEBUG'"
+    echo "UNK   = '$UNK'"
+fi
+
+if [[ -n "$UNK" ]] ; then
+    echo "FATAL:  Unknown option '$UNK'...exiting."
+    exit 2
+fi
+
+#if [[ -z $TEST  && $UID != "0" ]] ; then
+#  echo "You are not root!  Exiting...."
+#  exit
+#fi
+
 echo "Unpack '$ARCH'?"
-get_approval
+get_approval $YES
 
-tar -xvzf $ARCH
+# ensure we have an INSTDIR
+if [ ! -d $INSTDIR ] ; then
+    echo "WARNING:  no dir '$INSTDIR' found...create?."
+    get_approval
+    if [[ -n $TEST ]] ; then
+	echo "cmd: 'mkdir -p $INSTDIR'"
+    else
+	mkdir -p $INSTDIR
+    fi
+fi
+if [[ -n $TEST ]] ; then
+    echo "cmd: 'tar -xvzf $ARCH'"
+else
+    tar -xvzf $ARCH
+fi
 
-echo "Configuring..."
 
-# go to src dir
+
+echo "Configuring: going to dir '$ARCHDIR'..."
+
+# go to src dir ====================================
+pushd `pwd`
 cd $ARCHDIR
 
 # configure
-CONF=""
+CONF="perl Configure.pl --backend=moar --gen-moar --prefix=$INSTDIR"
 
 echo "config cmd: '$CONF'"
 echo "Configure?"
-get_approval
-$CONF
+get_approval $YES
+if [[ -n $TEST ]] ; then
+    echo "cmd: '$CONF'"
+else
+`echo $CONF`
+fi
 
+# go to config dir ====================================
+echo "Build?"
+get_approval $YES
+if [[ -n $TEST ]] ; then
+    echo "cmd: 'echo time make'"
+    echo "cmd: 'echo time make rakudo-test'"
+else
+    `echo time make`
+    `echo time make rakudo-test`
+fi
+
+echo "Install?"
+get_approval $YES
+if [[ -n $TEST ]] ; then
+    echo "cmd: 'echo time make install'"
+else
+`echo time make install`
+fi
+
+# final message
+cat <<HERE
+Installation complete.  Put the following lines in your environment:
+
+
+HERE
+
+exit 0
