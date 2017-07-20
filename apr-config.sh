532a5ad9fce4a9625ddaf1a5540195b8c1add51c
Tom Browder <tom.browder@gmail.com>
2017-07-20 16:39:43 -0500

    add new files

---
+++ apr-config.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 go"
+  echo
+  echo "  Configures Apr."
+  exit
+fi
+
+#  Optional Features:
+#    --disable-option-checking  ignore unrecognized --enable/--with options
+#    --disable-FEATURE       do not include FEATURE (same as --enable-FEATURE=no)
+#    --enable-FEATURE[=ARG]  include FEATURE [ARG=yes]
+#    --enable-layout=LAYOUT
+#    --enable-experimental-libtool Use experimental custom libtool
+#    --enable-shared[=PKGS]  build shared libraries [default=yes]
+#    --enable-static[=PKGS]  build static libraries [default=yes]
+#    --enable-fast-install[=PKGS]
+#                            optimize for fast installation [default=yes]
+#    --disable-libtool-lock  avoid locking (might break parallel builds)
+#    --enable-debug          Turn on debugging and compile time warnings
+#    --enable-maintainer-mode  Turn on debugging and compile time warnings
+#    --enable-profile        Turn on profiling for the build (GCC)
+#    --enable-pool-debug[=yes|no|verbose|verbose-alloc|lifetime|owner|all]    Turn on pools debugging
+#    --enable-malloc-debug   Switch on malloc_debug for BeOS
+#    --disable-lfs           Disable large file support on 32-bit platforms
+#    --enable-nonportable-atomics  Use optimized atomic code which may produce nonportable binaries
+#    --enable-threads        Enable threading support in APR.
+#    --enable-posix-shm      Use POSIX shared memory (shm_open) if available
+#    --enable-allocator-uses-mmap    Use mmap in apr_allocator instead of malloc
+#    --enable-allocator-guard-pages  Use guard pages in apr_allocator
+#                                    (implies --enable-allocator-uses-mmap)
+#    --enable-pool-concurrency-check Check for concurrent usage of memory pools
+#    --disable-dso           Disable DSO support
+#    --enable-other-child    Enable reliable child processes
+#    --disable-ipv6          Disable IPv6 support in APR.
+#
+#  Optional Packages:
+#    --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
+#    --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
+#    --with-pic[=PKGS]       try to use only PIC/non-PIC objects [default=use
+#                            both]
+#    --with-aix-soname=aix|svr4|both
+#                            shared library versioning (aka "SONAME") variant to
+#                            provide on AIX, [default=aix].
+#    --with-gnu-ld           assume the C compiler uses GNU ld [default=no]
+#    --with-sysroot[=DIR]    Search for dependent libraries within DIR (or the
+#                            compiler's sysroot if not specified).
+#    --with-installbuilddir=DIR location to store APR build files (defaults to '${datadir}/build')
+#    --without-libtool       avoid using libtool to link the library
+#    --with-efence[=DIR]     path to Electric Fence installation
+#    --with-valgrind[=DIR]   Enable code to teach valgrind about apr pools
+#                            (optionally: set path to valgrind headers)
+#    --with-sendfile         Override decision to use sendfile
+#    --with-egd[=DIR]        use EGD-compatible socket
+#    --with-devrandom[=DEV]  use /dev/random or compatible [searches by default]
+
+./configure
+
+# make
+# make install
+
+# NOW INSTALL APRUTIL
---
+++ apr-util-config.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# must check for its existence:
+APRPATH=/usr/local/apr
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 go"
+  echo
+  echo "  Configures Apr-util."
+  echo "  Expects Apr in '$APRPATH'."
+  exit
+fi
+
+if [[ ! -d $APRPATH ]] ; then
+    echo "FATAL:  Cannot find $APRPATH."
+    exit
+fi
+
+#  Optional Features:
+#    --disable-option-checking  ignore unrecognized --enable/--with options
+#    --disable-FEATURE       do not include FEATURE (same as --enable-FEATURE=no)
+#    --enable-FEATURE[=ARG]  include FEATURE [ARG=yes]
+#    --enable-layout=LAYOUT
+#    --disable-util-dso      disable DSO build of modular components (crypto,
+#                            dbd, dbm, ldap)
+#
+#  Optional Packages:
+#    --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
+#    --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
+#    --with-apr=PATH         prefix for installed APR or the full path to
+#                               apr-config
+#    --with-apr-iconv=DIR    relative path to apr-iconv source
+#    --with-crypto           enable crypto support
+#    --with-openssl=DIR      specify location of OpenSSL
+#    --with-nss=DIR          specify location of NSS
+#    --with-commoncrypto=DIR specify location of CommonCrypto
+#    --with-lber=library     lber library to use
+#    --with-ldap-include=path  path to ldap include files with trailing slash
+#    --with-ldap-lib=path    path to ldap lib file
+#    --with-ldap=library     ldap library to use
+#    --with-dbm=DBM          choose the DBM type to use.
+#                            DBM={sdbm,gdbm,ndbm,db,db1,db185,db2,db3,db4,db4X,db5X,db6X}
+#                            for some X=0,...,9
+#    --with-gdbm=DIR         enable GDBM support
+#    --with-ndbm=PATH        Find the NDBM header and library in `PATH/include'
+#                            and `PATH/lib'. If PATH is of the form `HEADER:LIB',
+#                            then search for header files in HEADER, and the
+#                            library in LIB. If you omit the `=PATH' part
+#                            completely, the configure script will search for
+#                            NDBM in a number of standard places.
+#    --with-berkeley-db=PATH Find the Berkeley DB header and library in
+#                            `PATH/include' and `PATH/lib'. If PATH is of the
+#                            form `HEADER:LIB', then search for header files in
+#                            HEADER, and the library in LIB. If you omit the
+#                            `=PATH' part completely, the configure script will
+#                            search for Berkeley DB in a number of standard
+#                            places.
+#    --with-pgsql=DIR        specify PostgreSQL location
+#    --with-mysql=DIR        enable MySQL DBD driver
+#    --with-sqlite3=DIR      enable sqlite3 DBD driver
+#    --with-sqlite2=DIR      enable sqlite2 DBD driver
+#    --with-oracle-include=DIR
+#                            path to Oracle include files
+#    --with-oracle=DIR       enable Oracle DBD driver; giving ORACLE_HOME as DIR
+#    --with-odbc=DIR         specify ODBC location
+#    --with-expat=DIR        specify Expat location
+#    --with-iconv=DIR        path to iconv installation
+
+# may have to add paths to one or more of the options below
+#    --with-apr=PATH         prefix for installed APR or the full path to
+#    --with-pgsql=DIR        specify PostgreSQL location
+#    --with-sqlite3=DIR      enable sqlite3 DBD driver
+#    --with-berkeley-db=PATH Find the Berkeley DB header and library in
+#    --with-gdbm=DIR         enable GDBM support
+#    --with-ndbm=PATH        Find the NDBM header and library in `PATH/include'
+./configure          \
+--with-apr=$APRPATH  \
+--with-pgsql         \
+--with-sqlite3       \
+--with-berkeley-db   \
+--with-gdbm          \
+--with-ndbm
+
+# make
+# make install
+
+# create file
+#   /etc/ld.so.conf.d/apr.conf
+# containing the line:
+#   /usr/local/apr/lib
+# then execute:
+#   ldconfig
