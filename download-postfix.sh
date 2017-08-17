09aac9b40a3c9064491729c766d93c3e7468cb13
Tom Browder <tom.browder@gmail.com>
2017-08-17 10:20:27 -0500

    postfix downloader working ok now, others not ready and have safety and warning exits

--- download-postfix.sh
+++ download-postfix.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-# a regex to determine e or o
-re="[0-9]+\.[0-9]+\.[0-9]+"
-if [[ $1 =~ $re ]] ; then
-    echo "$1 is an official release"
-else 
-    echo "$1 is an experimental release"
-fi
-exit
-
 # set the latest known version
 LATEST=3.3-20170730
-# set the type (o - official, e - experimental)
-TYPE=e
 
 SITE="ftp://ftp.porcupine.org/mirrors/postfix-release"
-if [[ $TYPE = 'e
-SRCDIR=$SITE/official
-SRCDIR=$SITE/experimental
-KEY=$SITE/wietse.pgp
-
 if [[ -z $1 ]] ; then
-    echo "Usage: $0 <version> [v]"
+    echo "Usage: $0 go | <version> [v]"
     echo
     echo "Downloads postfix source and signature data from site '$SITE'."
     echo
+    echo "The 'go' uses version '$LATEST', or you can choose another version."
     echo "The 'v' verifies only." 
+    echo
     exit
 fi
 
-VER=$1
+if [[ $1 =~ "g" ]] ; then
+    VER=$LATEST
+    #echo "first arg ($1) is 'go'"
+else
+    VER=$1
+    #echo "first arg ($1) should be a version"
+fi
+#echo "debug exit"; exit
+
 VERIFY=$2
- 
-SNAME=postfix-$VER.tar.tgz
-GNAME=postfix-$VER.tar.gz.gpg2 
+
+# a regex to determine e or o
+re="[0-9]+\.[0-9]+\.[0-9]+"
+if [[ $VER =~ $re ]] ; then
+    echo "$VER is an official release"
+    SRCDIR=$SITE/official
+else 
+    echo "$VER is an experimental release"
+    SRCDIR=$SITE/experimental
+fi
+
+SNAME=postfix-$VER.tar.gz
+GNAME="${SNAME}.gpg2" 
 KNAME=wietse.pgp
-SRC="$SRCDIR/postfix-$VER.tar.gz"
-GPG="$SRCDIR/postfix-$VER.tar.gz.gpg2" 
+
+SRC="$SRCDIR/$SNAME"
+GPG="$SRCDIR/$GNAME" 
+KEY="$SITE/$KNAME"
 
 if [[ -z $VERIFY ]] ; then
 
