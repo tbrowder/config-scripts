f8b7df77429853258e5c200631575a7f9d9dfc7b
Tom Browder <tom.browder@gmail.com>
2017-08-18 10:01:21 -0500

    much more prettying and safing per Zoffix's comments on IRC #perl6

--- rakudo-star-config.sh
+++ rakudo-star-config.sh
@@ -18,21 +18,47 @@ if [[ -z "$1" ]] ; then
     echo
     echo "For more thorough, but longer, testing run:"
     echo
-    echo "  # make rakudo-spectest"
-    echo "  # make module-test"
+    echo "  # make rakudo-spectest   # <= expect 24 min or more "
+    echo "  # make modules-test      # <= expect 11 min or more"
     echo
     echo "Finally, install into '$PREFIX' by running:"
     echo
-    echo "  # make install"
+    echo "  # make install           # <= expect 6 min or more"
     echo
     exit
 fi
 
 # must remove old binaries and libs
-rm -rf $PREFIX/bin
-rm -rf $PREFIX/include
-rm -rf $PREFIX/lib
-rm -rf $PREFIX/share/nqp
-rm -rf $PREFIX/share/pkgconfig
+
+SUBDIRS="
+bin
+include
+lib
+share/nqp
+share/pkgconfig
+"
+
+echo
+
+RM=
+for subdir in $SUBDIRS
+do
+
+    if [[ -d "$PREFIX/$subdir" ]] ; then
+	if [[ -z $RM ]] ; then
+	    echo "WARNING: Found one or more existing subdirectories to remove:"
+	    echo
+	    RM=y
+	fi
+	echo "  $PREFIX/$subdir"
+    fi
+done
+
+if [[ -n $RM ]] ; then
+    echo
+    echo "FATAL:    The above listed existing subdirectories in '$PREFIX'"
+    echo "           must be removed before continuing."
+    exit
+fi
 
 perl Configure.pl --gen-moar --prefix=$PREFIX
