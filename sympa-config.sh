956a83bbf8e6f01e59c9d1bcbd8b11e3d3dc3f02
Tom Browder <tom.browder@gmail.com>
2019-03-05 16:02:58 -0600

    fix my std sympa dirs

--- sympa-config.sh
+++ sympa-config.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+PREFIX=/usr/local/sympa
 if [ -z "$1" ] ; then
   echo "Usage: $0 go"
   echo
-  echo "  Configures Sympa"
+  echo "  Configures Sympa into dir '$PREFIX'"
   exit
 fi
 
 #  Optional Features:
+MAKEMAP=$PREFIX/makemap
+NEWALIASES=$PREFIX//newaliases
+POSTALIAS=$PREFIX/postalias
+POSTMAP=$PREFIX/postmap
 
-./configure
+./configure \
+  --enable-fhs \
+  --prefix=/usr/local/sympa \
+  --with-confdir=/etc/sympa \
+  --without-initdir \
+  --with-unitsdir=/lib/systemd/system \
+               \
+  --with-makemap=$MAKEMAP \
+  --with-newaliases=$NEWALIASES \
+  --with-postalias=$POSTALIAS \
+  --with-postmap=$POSTMAP
 
 # make
 # sudo make install
