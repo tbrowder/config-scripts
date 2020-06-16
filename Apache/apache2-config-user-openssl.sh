8cafd64dcb0c1abcb1ba61efd85609857c8f988c
Tom Browder <tom.browder@gmail.com>
2020-06-16 06:31:39 -0500

    add sysd lib dev

--- Apache/apache2-config-user-openssl.sh
+++ Apache/apache2-config-user-openssl.sh
@@ -67,6 +67,7 @@ fi
 #     libtool libexpat1-dev libxml2-dev
 #     liblua5.2-dev liblua5.2-0 lua5.2
 #     libpcre3-dev
+#     libsystemd-dev
 #
 #   The following are NOT needed if APR is installed from source:
 #     libaprutil1-dbd-pgsql
