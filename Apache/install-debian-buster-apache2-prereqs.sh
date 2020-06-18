a4a51ebd0f426d70aeb9e4e26939a963f538e99c
Tom Browder <tom.browder@gmail.com>
2020-06-18 15:09:59 -0500

    impove docs and add another (unfinished) script

---
+++ Apache/install-debian-buster-apache2-prereqs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ; then
+    echo "Usage: $0 go
+    echo
+    echo "Run as root to install Apache2 prereq packages for Debian 10 Buster amd64"
+    echo
+    exit
+fi
+
+aptitude update
+
+aptitude install \
+  ntp  \
+  ntp-doc  \
+  ntpdate \
+  libtool  \
+  libexpat1-dev  \
+  libxml2-dev \
+  liblua5.2-dev  \
+  liblua5.2-0  \
+  lua5.2 \
+  libpcre3-dev \
+  libsystemd-dev \
+  libwepsocketpp-dev \
+  libjannson-dev \
+  libnghttp2-dev \
+  libidn2-dev \
+  librtmp-dev \
+  libssh2-1-dev \
+  libpsl-dev \
+  krb5-multidev \
+  comerr-dev \
+  libldap2-dev \
