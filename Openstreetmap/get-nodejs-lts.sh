ce5ee67a8392195ac29f6dfe2a82aa027b6b25db
Tom Browder <tom.browder@gmail.com>
2020-07-16 09:55:25 -0500

    add correct file

---
+++ Openstreetmap/get-nodejs-lts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ; then
+    echo "Usage: $0 go"
+    echo
+    echo "Run, as root, to download and install nodejs LTS (v12)"
+    echo 
+    echo "See github.com/nodesource/distributions"
+    echo
+    exit
+fi
+
+curl -sL https://deb.nodesource.com/setup_lts.x | bash -
+#curl -o setup_lts.x -sL https://deb.nodesource.com/setup_lts.x 
+
+#apt-get install nodejs
