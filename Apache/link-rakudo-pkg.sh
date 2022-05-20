d766b38aae98b9b72ed1d05171ab19b504e9a5e9
Tom Browder <tbrowder@acm.org>
2022-05-20 07:21:22 -0500

    update msg

--- Apache/link-rakudo-pkg.sh
+++ Apache/link-rakudo-pkg.sh
@@ -3,14 +3,19 @@
 if [ -z "$1" ] ; then
   echo "Usage: $0 go"
   echo
-  echo "  For users of 'rakudo-pkg' installations: installs"
-  echo "    the 'raku' executable as a sym link to    '/usr/local/raku'."
-  echo "    and the 'zef' executable as a sym link to '/usr/local/zef'."
+  echo "  For users of 'rakudo-pkg' raku installations, installs the"
+  echo "  the following executables as sym links in '/usr/local/bin':"
+  echo "    raku"
+  echo "    rakudo"
+  echo "    perl6"
+  echo "    zef"
   echo
-  echo "  (This must be executed as the root user.)"
+  echo "  (This script must be executed as the root user.)"
   echo
   exit
 fi
 
+ln -sf /opt/rakudo-pkg/bin/rakudo       /usr/local/bin/rakudo
 ln -sf /opt/rakudo-pkg/bin/raku         /usr/local/bin/raku
+ln -sf /opt/rakudo-pkg/bin/perl6        /usr/local/bin/perl6
 ln -sf /opt/rakudo-pkg/var/zef/bin/zef  /usr/local/bin/zef
