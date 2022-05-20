f0d2c00196e0c1bfe86f59cd04575acfa0bf4fc0
Tom Browder <tbrowder@acm.org>
2022-05-20 06:31:19 -0500

    provide a link for sudo use

---
+++ Apache/link-rakudo-pkg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 go"
+  echo
+  echo "  For users of 'rakudo-pkg' installations: installs"
+  echo "    the 'raku' executable as a sym link to '/usr/local/raku'."
+  echo
+  echo "  (This must be executed as the root user.)"
+  echo
+  exit
+fi
+
+ln -sf /opt/rakudo-pkg/bin/raku /usr/local/bin/raku
